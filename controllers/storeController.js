8ef5d3b210c9b9caa05b7854f8b266e729997329
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2018-02-16 07:18:37 -0500

    Create and Update Store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store'); 
+
+
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
@@ -7,6 +11,44 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
-};
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+}; 
+
+exports.getStores = async (req, res) => {
+  //need query DB for stores
+  const stores = await Store.find();
+  console.log(stores);
+
+  res.render('stores', {title: 'Stores', stores});
+}
+
+
+exports.editStore = async (req, res) => {
+  //find store id
+  // res.json(req.params); to test
+  const store = await Store.findOne({ _id: req.params.id });
+  // res.json(store); to test
+
+  //confirm owner 
+
+
+  //render edit form to update
+  res.render('editStore', { title: `Edit ${store.name}`, store});
+}
+
+exports.updateStore = async (req, res) => {
+  //find and update store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, 
+    { new: true,
+      runValidators: true
+    }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`);
+    res.redirect(`/stores/${store._id}/edit`);
+
+
+
+  //redirect them the store and tell them worked
+}
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-router.get('/', storeController.homePage);
-router.get('/add', storeController.addStore);
 
-router.post('/add', storeController.createStore); 
+const { catchErrors } = require('../handlers/errorHandlers');
+
+router.get('/', catchErrors(storeController.getStores)); //also wrapping this due to async
+router.get('/stores', storeController.getStores);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore)); //wrap in helper function -- to catch errors. 
+router.post('/add/:id', catchErrors(storeController.updateStore)); //wrap in helper function -- to catch errors. 
+router.get('/stores/:id/edit', catchErrors(storeController.editStore)); 
 
 module.exports = router;
